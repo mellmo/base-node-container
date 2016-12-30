28eb44f964a7a7e4c779e5bad7f7df0d7f2a908d
Mohamed Mellouki <mellouki.mhd@gmail.com>
2016-12-29 20:32:10 -0500

    closes #2, server up and runing

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "standard",
+    "installedESLint": true,
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {
+        "semi": ["error", "always"]
+    }
+};
